4193c734fd70696140fff5fdc03a6627021c4cfd
ursgarsan <ugsugs333@gmail.com>
2026-02-05 17:26:52 +0100

    CHORE: mejora estilos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,30 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import Lara from '@primeuix/themes/lara';
+import { definePreset } from '@primeuix/themes';
 import { routes } from './app.routes';
+
+const CustomPreset = definePreset(Lara, {
+  semantic: {
+    primary: {
+      50: '{slate.50}',
+      100: '{slate.100}',
+      200: '{slate.200}',
+      300: '{slate.300}',
+      400: '{slate.400}',
+      500: '{slate.500}',
+      600: '{slate.600}',
+      700: '{slate.700}',
+      800: '{slate.800}',
+      900: '{slate.900}',
+      950: '{slate.950}'
+    }
+  },
+  variables: {
+    '--p-slider-handle-content-background': '#3B3B1A'
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,9 +35,12 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideAnimations(),
     providePrimeNG({
-            theme: {
-                preset: Aura
-            }
-        })
+      theme: {
+        preset: CustomPreset,
+        options: {
+          darkModeSelector: false
+        }
+      }
+    })
   ]
 };
